ca3aab82c17b7b0457ca559b9f8240e0d1ba6cd9
Jorge <jorge@jvflux>
2018-10-30 23:37:41 -0400

    refactorizacion

--- index.js
+++ index.js
@@ -45,6 +45,24 @@ global.gTests = [{
         enabled: true,
     }]
 
+// let transporter = nodemailer.createTransport({
+//     host: 'smtp.ethereal.email',
+//     port: 587,
+//     secure: false, // true for 465, false for other ports
+//     auth: {
+//         user: account.user, // generated ethereal user
+//         pass: account.pass // generated ethereal password
+//     }
+// });
+
+// // setup email data with unicode symbols
+// let mailOptions = {
+//     from: '"Fred Foo 👻" <foo@example.com>', // sender address
+//     to: 'bar@example.com, baz@example.com', // list of receivers
+//     subject: 'Hello ✔', // Subject line
+//     text: 'Hello world?', // plain text body
+//     html: '<b>Hello world?</b>' // html body
+// };
 
 function fetchResults(URL, CHECKSSL, TIMEOUT) {
     return new Promise((resolve, reject) =>{
@@ -266,21 +284,3 @@ for (test of global.gTests){
     
 
 
-// let transporter = nodemailer.createTransport({
-//     host: 'smtp.ethereal.email',
-//     port: 587,
-//     secure: false, // true for 465, false for other ports
-//     auth: {
-//         user: account.user, // generated ethereal user
-//         pass: account.pass // generated ethereal password
-//     }
-// });
-
-// // setup email data with unicode symbols
-// let mailOptions = {
-//     from: '"Fred Foo 👻" <foo@example.com>', // sender address
-//     to: 'bar@example.com, baz@example.com', // list of receivers
-//     subject: 'Hello ✔', // Subject line
-//     text: 'Hello world?', // plain text body
-//     html: '<b>Hello world?</b>' // html body
-// };
